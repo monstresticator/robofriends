1b1808ebd59d3b2e94a72037d2575d9a2f311713
Buenny Fry <bfry3@Monstresticators-Mac-mini.local>
2019-03-24 04:53:31 -0500

    add data to db.json file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-		   robots: robots,
+		   robots: [],
            searchfield: ''	
 		}
 	}
+
+    componentDidMount() {
+    	fetch('https://jsonplaceholder.typicode.com/users')
+    	   .then(response => .json())
+    	   .then(users => {this.setState({ robots: users })});
+    }
+
     // anytime you make your own methods, use arrow functions
     onSearchChange = (event) => {
     	this.setState({ searchfield: event.target.value })
@@ -22,14 +29,19 @@ class App extends Component {
 	render() {
 	   const foundRobots = this.state.robots.filter(robots => {
     		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    	})
-	   return (
-	      <div className='tc'>
-             <h1 className='f1'>RoboFriends</h1>
-             <SearchBox searchChange={ this.onSearchChange }/>
-             <CardList robots={ foundRobots } />
-          </div>
-       );
+       })
+
+       if (this.state.robots.length === 0) {
+       	 return <h1>Please wait ...</h1>
+       } else {
+	     return (
+	       <div className='tc'>
+              <h1 className='f1'>RoboFriends</h1>
+              <SearchBox searchChange={ this.onSearchChange }/>
+              <CardList robots={ foundRobots } />
+           </div>
+         );
+       }
     }
 }
 
