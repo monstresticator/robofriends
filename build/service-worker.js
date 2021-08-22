0497da3015cfdea7ef2c93ee41d82d7f2b888ecb
Monstresticator <bfry3611@gmail.com>
2021-08-22 01:32:08 -0500

    update app

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.715ff95a0201a1bf38bf88d8346ccaa3.js"
+  "/precache-manifest.cc7b3b2c006a7ab4fed41aa633e5cfdf.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
